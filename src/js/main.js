469c4c8f7342dfadbdb6788b7d9e2cb19fd23611
Sandra S <zandrastr@hotmail.com>
2023-03-04 15:35:50 +0100

    added dark mode feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log("Test!");
+let header = document.querySelector(".header");
+let toggleBtn = document.querySelector(".toggle-button");
+let plantSections = document.querySelector(".plant-sections-wrapper");
+let footer = document.querySelector(".footer");
+let bodyToggle = document.body;
+
+function toggleTheme () {
+    console.log("Button is pressed!");     
+    header.classList.toggle("header-dark-mode");
+    bodyToggle.classList.toggle("body-dark-mode");
+    plantSections.classList.toggle("text-dark-mode");
+    footer.classList.toggle("footer-dark-mode");
+};
+
+toggleBtn.addEventListener("click", toggleTheme);
+
